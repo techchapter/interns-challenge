1cf422117f3b8bc7b137aa4f8cb978c786a193cd
HalahRamadan <s193731@student.dtu.dk>
2024-05-06 18:55:31 +0200

    add output of runnig code

--- Program.cs
+++ Program.cs
@@ -5,7 +5,8 @@ namespace interns_challenge
     class Program
     {
         static void Main(string[] args)
-        {
+        { 
+            // implement 
             HolidayCalendar.HolidayCalendar calendar = new HolidayCalendar.HolidayCalendar();
             
             DateTime testDate = new DateTime(2024, 12, 25);
@@ -25,3 +26,16 @@ namespace interns_challenge
         }
     }
 }
+/*
+here is what i got after run code 
+Is 12/25/2024 12:00:00 AM a holiday? False
+Holidays in 2024:    
+1/1/2024 12:00:00 AM 
+3/28/2024 12:00:00 AM
+3/29/2024 12:00:00 AM
+3/31/2024 12:00:00 AM
+5/9/2024 12:00:00 AM
+5/19/2024 12:00:00 AM
+5/20/2024 12:00:00 AM
+6/5/2024 12:00:00 AM
+*/
